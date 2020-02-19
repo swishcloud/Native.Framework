fd0ac7ed3211808439126101592437d8313e1671
kotone <kotone@email.com>
2020-02-19 11:47:42 +0800

    文件排版

--- docs/1.0/config.js
+++ docs/1.0/config.js
@@ -8,32 +8,32 @@ let config = {
         },
         {
             title: '开始使用', type: 'dropdown', items: [
-				{
-					title: '快速入门', path: '/QuickStart'
-				},
-				{
-					title: '应用配置', path: '/Configuration'
-				},
-				{
-					title: '实现接口', path: '/Register'
-				},
-				{
-					title: '编译部署', path: '/Build'
-				},
-				{
-					title: '应用调试', path: '/Debug'
-				}
-			]
+                {
+                    title: '快速入门', path: '/QuickStart'
+                },
+                {
+                    title: '应用配置', path: '/Configuration'
+                },
+                {
+                    title: '实现接口', path: '/Register'
+                },
+                {
+                    title: '编译部署', path: '/Build'
+                },
+                {
+                    title: '应用调试', path: '/Debug'
+                }
+            ]
         },
         {
             title: '进阶使用', type: 'dropdown', items: [
                 {
                     title: '增加窗体', path: '/Window'
                 },
-				{
+                {
                     title: '增加悬浮窗', path: '/Flow'
                 }
-			]
+            ]
         },
         {
             title: '版本升级', path: '/Upgrade'
@@ -49,14 +49,14 @@ let config = {
             ]
         },
         {
-			title: '范例大全', type: 'dropdown', items: [
+            title: '范例大全', type: 'dropdown', items: [
                 {
                     title: '目录', path: '/Example'
                 },
                 {
                     title: '实作', path: 'https://github.native.run/Jie2GG/Native.Framework/tree/Example'
                 }
-		    ]
+            ]
         },
         {
             title: '酷Q文库', path: 'https://docs.cqp.im/'
